1cc0374fa68be30f83295caed03380f2cbd2b7b1
avatarchristine <catfelizardo@gmail.com>
2016-01-03 23:09:30 +0800

    Added CURSOR site

---
+++ CURSOR/cursor_js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+	$("#homelink").click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		},500);
+		return false;
+	});
+
+	$("#orglink").click(function() {
+		$('html, body').animate({
+		   	scrollTop: $("#org").offset().top
+		},500);
+		return false;
+	});
+
+	$("#memslink").click(function() {
+		$('html, body').animate({
+		   	scrollTop: $("#mems").offset().top
+		},500);
+		return false;
+	});
+
+	$("#eventslink").click(function() {
+		$('html, body').animate({
+		   	scrollTop: $("#events").offset().top
+		},500);
+		return false;
+	});
+
+	$("#contactlink").click(function() {
+		$('html, body').animate({
+		   	scrollTop: $("#contact").offset().top
+		},500);
+		return false;
+	});
+
+});
